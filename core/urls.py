c3c43856e7fad6802bf7845b97f8b10957c9dfba
ramazon07-cmd <ustau2453@gmail.com>
2024-11-21 20:03:36 +0500

    added

--- core/urls.py
+++ core/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('health/', lambda _: JsonResponse({'detail': 'Healthy'}), name='health'),
     path('users/', include('users.urls')),
     path('', include('articles.urls')),
+    path('', lambda _: JsonResponse({'detail': 'Healthy'}), name='health'),
     path('schema/', user_passes_test(is_superuser)(SpectacularAPIView.as_view()), name='schema'),
     path('swagger/', user_passes_test(is_superuser)(SpectacularSwaggerView.as_view()), name='swagger-ui'),
     path('redoc/', user_passes_test(is_superuser)(SpectacularRedocView.as_view()), name='redoc'),
