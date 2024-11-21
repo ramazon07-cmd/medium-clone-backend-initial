670c399513bce9ebdc964869da5787fe869b7c6e
ramazon07-cmd <ustau2453@gmail.com>
2024-11-21 20:47:04 +0500

    dd

--- core/settings.py
+++ core/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [config('ALLOWED_HOSTS')]
 
 # Application definition
 
