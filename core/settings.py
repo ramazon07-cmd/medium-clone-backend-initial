1134d37b4ae00bb7006241e4a2d4f2714395761c
ramazon07-cmd <ustau2453@gmail.com>
2024-11-22 17:09:55 +0500

    dddf

--- core/settings.py
+++ core/settings.py
@@ -258,7 +258,7 @@ LOCALE_PATHS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 STATIC_ROOT = BASE_DIR / "static"
 MEDIA_URL = '/media/'
