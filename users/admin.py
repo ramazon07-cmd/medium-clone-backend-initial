d45ad1ef5217044435379bfb620bd8235860a593
ramazon07-cmd <ustau2453@gmail.com>
2024-11-23 20:44:54 +0500

    80

--- users/admin.py
+++ users/admin.py
@@ -2,12 +2,27 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Recommendation, Pin, Notification
 
+# @admin.register(CustomUser)
+# class CustomUserAdmin(UserAdmin):
+#     model = CustomUser
+#     list_display = ('username', 'first_name', 'last_name', 'email', 'is_staff', 'is_active', 'birth_year', 'middle_name', 'avatar')
+#     search_fields = ('username', 'first_name', 'last_name', 'email')
+#     ordering = ('username',)
+
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ('username', 'first_name', 'last_name', 'email', 'is_staff', 'is_active', 'birth_year', 'middle_name', 'avatar')
-    search_fields = ('username', 'first_name', 'last_name', 'email')
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'middle_name')
+    list_display_links = ('id', 'username', 'email')
+    search_fields = ('username', 'email', 'first_name', 'last_name', 'middle_name')
+    list_filter = ('last_login', 'date_joined', 'is_staff', 'is_superuser', 'is_active')
     ordering = ('username',)
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'middle_name', 'email', 'avatar', 'birth_year')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
 
 admin.site.register(Recommendation)
 admin.site.register(Pin)
