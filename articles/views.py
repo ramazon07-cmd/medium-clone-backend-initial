d8cf0d4774d3ee6f187dec63bb3c890bfed8ae24
ramazon07-cmd <ustau2453@gmail.com>
2024-11-29 21:24:20 +0500

    o

--- articles/views.py
+++ articles/views.py
@@ -100,37 +100,12 @@ class ArticlesView(viewsets.ModelViewSet):
         serializer = self.get_serializer(self.queryset, many=True)
         return Response(serializer.data)
 
-    # def destroy(self, request, *args, **kwargs):
-    #     article = self.get_object()
-    #     article.status = "trash"
-    #     article.save()
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
-
     def destroy(self, request, *args, **kwargs):
-        try:
-            article = self.get_object()
-            if not (article.author == request.user or request.user.has_perm('articles.delete_article')):
-                return Response(
-                    {"detail": "You do not have permission to delete this article."},
-                    status=status.HTTP_403_FORBIDDEN
-                )
+        article = self.get_object()
+        article.status = "trash"
+        article.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
-            article.status = "trash"
-            article.save()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-
-        except Article.DoesNotExist:
-            return Response(
-                {"detail": "Article not found."},
-                status=status.HTTP_404_NOT_FOUND
-            )
-
-        except Exception as e:
-            return Response(
-                {"detail": f"An unexpected error occurred: {str(e)}"},
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR
-            )
-            
 class TopicFollowView(viewsets.ViewSet):
     def create(self, request, topic_id=None):
         user = request.user
