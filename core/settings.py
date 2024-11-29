ddb34a718ff00c3240e3b5a9417415530691fb45
ramazon07-cmd <ustau2453@gmail.com>
2024-11-29 17:50:34 +0500

    mmm

--- core/settings.py
+++ core/settings.py
@@ -202,19 +202,18 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': config('DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': config('POSTGRES_DATABASE', default='db.sqlite3'),
-        'USER': config('POSTGRES_USER', default=''),
-        'PASSWORD': config('POSTGRES_PASSWORD', default=''),
-        'HOST': config('POSTGRES_HOST', default=''),
-        'PORT': config('DB_PORT', default=''),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT"),
         'OPTIONS': {
             'sslmode': 'require',
         },
     }
 }
-
 
 
 # Password validation
