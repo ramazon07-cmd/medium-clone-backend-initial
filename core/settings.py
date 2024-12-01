e430fe6b357e14475425e4ca65e3b1a7bce50a5a
ramazon07-cmd <ustau2453@gmail.com>
2024-12-01 18:21:29 +0500

    o

--- core/settings.py
+++ core/settings.py
@@ -161,7 +161,8 @@ logger.info(f"Using redis | URL: {REDIS_URL}")
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': REDIS_URL,
+        # 'LOCATION': REDIS_URL,
+        'LOCATION': 'redis://127.0.0.1:6379/1',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
